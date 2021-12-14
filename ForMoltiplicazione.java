32cc45c32501afbd3f301944b381aa3a7ead6ab1
githubsnk <giuvarca@gmail.com>
2021-12-14 11:53:21 +0100

    2)ForMoltiplicazione

--- ForMoltiplicazione.java
+++ ForMoltiplicazione.java
@@ -1,5 +1,5 @@
 package JavaSchool;
-/****************************Pseudocode:**********************************
+/***************************Pseudocode:***********************************
 
 
 Scrivi("Digita il primo operatore")
@@ -7,35 +7,31 @@ Op1=<--
 Scrivi("Digita il secondo operatore")
 Op2=<--
 
-	if (Op1>0 AND Op2>0)
+	if (Op1>0 And Op2>0 OR Op1<0 And Op2<0)
 		Then
-			For I:=1 to Op2
-			k=k+Op1
+		 if (Op1<0 AND Op2<0){ //se Op1 e Op2 sono negativi
+		 Then
+            Op1= -Op1;  //Trasformo Op1 nel suo opposto positivo
+            Op2= -Op2;}  // Trasformo Op2 nel suo opposto positivo
+      
+        for (int I=1; I<=Op2; I++){
+            k=k+Op1;}}  // somma Op1 per n volte pari al valo
 			End For	
 					
-		Else if (op1<0 AND Op2>0)
-			THEN
-				Op1Ass= Op1-Op1-Op1
-				For I:=1 to Op2
-				K=K+Op1Ass
-				End For	
-				k=-k
-			
-			Else if (Op2<0) AND (Op1>0)
-				THEN
-					Op2Ass= op2-op2-op2
-					For I:=1 to Op2Ass
-					K=K+Op1
-					End For	
-					k=-k
-			
-				Else if (Op1<0 AND Op2<0)
-					THEN
-						Op2Ass= op2-op2-op2
-						Op1Ass= op1-op1-op1
-						For I:=1 to Op2Ass
-						k=k+Op1Ass
-						End For	
+	else if (Op1<0 OR Op2<0){  //Altrimenti se Op1 oppure Op2 sono negativi
+          
+          if (Op1<0){  //se Op1 è negativo
+          Then
+            Op1= -Op1;}  //Trasformo Op1 nel suo opposto positivo
+          
+          else if (Op2<0){  // altrimenti se Op2 è negativo
+          Then
+            Op2= -Op2;}  // Trasformo Op2 nel suo opposto positivo
+                                 
+            for (int I=1;I<=Op2; I++){ 
+                k=k+Op1;}  //somma Op1 assoluto per n volte pari al valore di Op2
+            End For
+            k=-k;}  //Ripristina il segno -
 End If		
 *******************************************************************************/
 import java.util.*;
@@ -43,41 +39,41 @@ public class ForMoltiplicazione
 {
   public static void main (String[]args)
   {
-    Scanner scn = new Scanner(System.in);
-int k=0, Op1=0, Op2=0, Op1Ass, Op2Ass;
+   Scanner scn = new Scanner(System.in);
+int k=0, Op1=0, Op2=0;
 
-System.out.println("Digita il primo operatore");
+System.out.println("Digita il PRIMO OPERATORE");
 Op1= scn.nextInt();
-System.out.println("Digita il secondo operatore");
+System.out.println("Digita il SECONDO OPERATORE");
 Op2= scn.nextInt();
 
-    if (Op1>0 && Op2>0){ //Se Op1 e Op2 sono num positivi
+    if (Op1>0 && Op2>0 || Op1<0&&Op2<0){ //Se Op1 e Op2 sono positivi oppure se Op1 e Op2 sono negativi
+        if (Op1<0&&Op2<0){//se Op1 e Op2 sono negativi
+            Op1= -Op1;//Trasformo Op1 nel suo opposto positivo
+            Op2= -Op2;}// Trasformo Op2 nel suo opposto positivo
+          
         for (int I=1; I<=Op2; I++){
-        k=k+Op1; // somma Op1 per n volte pari al Op2
-        }
-    } else if (Op1<0 && Op2>0){//Altrimenti se Op1 e Negativo e Op2 è positivo
-        Op1Ass= Op1-Op1-Op1;//Trasformo Op1 nel suo opposto positivo
+            k=k+Op1;}} // somma Op1 per n volte pari al valore di Op2
+        
+     else if (Op1<0 || Op2<0){//Altrimenti se Op1 oppure Op2 sono negativi
+          
+          if (Op1<0){ // se Op1 è negativo
+            Op1= -Op1;}//Trasformo Op1 nel suo opposto positivo
+          
+          else if (Op2<0){// altrimenti se Op2 è negativo
+            Op2= -Op2;}// Trasformo Op2 nel suo opposto positivo
+                                 
             for (int I=1;I<=Op2; I++){ 
-            k=k+Op1Ass;// somma Op1 assoluto per n volte pari al Op2
-            }
-        k=-k; //Ripristina il segno -
-    } else if (Op2<0 && Op1>0){ //Altrimenti se Op2 è negativo e Op1 è positivo
-        Op2Ass= Op2-Op2-Op2;// Trasformo Op2 nel suo opposto positivo
-            for (int I=1; I<=Op2Ass; I++){
-            k=k+Op1; // somma il primo operatore per n volte pari al Op2
-            }
-        k=-k; //Ripristina il segno -
-    } else if (Op1<0 && Op2<0){//Altrimenti se Op1 e Op2 sono negativi
-        Op1Ass= Op1-Op1-Op1;// Trasformo Op1 nel suo opposto positivo
-        Op2Ass= Op2-Op2-Op2;// Trasformo Op2 nel suo opposto positivo
-            for (int I=1; I<=Op2Ass; I++){
-                k=k+Op1Ass;// somma Op1 assoluto per n volte pari al Op2
-        }
-    }
+                k=k+Op1;}// somma Op1 assoluto per n volte pari al valore di Op2
+            
+            k=-k;} //Ripristina il segno -
+        
 System.out.println("Il risultato del prodotto è: ");
 System.out.println(k);
+scn.close();
     }
 }
+
 
 
 
